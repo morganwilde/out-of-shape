b4f5236a07f3885d53a1c01665506ddbe832568d
flaredshadow <flaredlight@aim.com>
2015-12-03 19:08:35 -0800

    p1 body

--- public/js/Player1Class.js
+++ public/js/Player1Class.js
@@ -18,11 +18,12 @@ Player1Class.prototype.initWithDimensionsAndArena = function(width, height, dept
     this.collider.getNode().name = "P1 Collider";
 
     // character body
-    var headSizeInRelationToBody = 0.25;
-    var geometry = new THREE.BoxGeometry(
-        width, 
-        height, 
-        depth
+    var headSizeInRelationToBody = 0.3;
+    var armSizeInRelationToBody = 0.25;
+    var geometry = new THREE.SphereGeometry(
+        width/2, 
+        100, 
+        100
     );
     var material = new THREE.MeshPhongMaterial({
         color: this.bodyColor,
@@ -32,16 +33,83 @@ Player1Class.prototype.initWithDimensionsAndArena = function(width, height, dept
         shading: THREE.FlatShading
     });
 
-    var head = new THREE.Mesh(geometry, material);
-    head.name = "P1 Body";
-    head.position.x = 0;
-    head.position.y = 0;
+    var body = new THREE.Mesh(geometry, material);
+    body.name = "P1 Body";
+    body.position.x = 0;
+    body.position.y = 0;
+    body.position.z = 0;
+    
+    body.castShadow = true;
+    body.receiveShadow = true;
+    this.collider.getNode().add(body);
+
+    var headGeometry = new THREE.SphereGeometry(
+        headSizeInRelationToBody * width, 
+        100, 
+        100
+    );
+
+    var headMaterial = new THREE.MeshPhongMaterial({
+        color: 0xc0392b,
+        wireframe: false,
+        specular: 0x000000, 
+        shininess: 0, 
+        shading: THREE.FlatShading
+    });
+
+    var head = new THREE.Mesh(headGeometry, headMaterial);
+    head.name = "P1 Head";
+    head.position.x = .6;
+    head.position.y = 7;
     head.position.z = 0;
     
     head.castShadow = true;
     head.receiveShadow = true;
     this.collider.getNode().add(head);
 
+    var legs = new THREE.Mesh(geometry, material);
+    legs.name = "P1 Legs";
+    legs.position.x = 0;
+    legs.position.y = -4;
+    legs.position.z = 0;
+    
+    legs.castShadow = true;
+    legs.receiveShadow = true;
+    this.collider.getNode().add(legs);
+
+    var armGeometry = new THREE.CylinderGeometry(
+        3, 
+        3, 
+        armSizeInRelationToBody * width
+    );
+
+    var armMaterial = new THREE.MeshPhongMaterial({
+        color: 0x16a085,
+        wireframe: false,
+        specular: 0x000000, 
+        shininess: 0, 
+        shading: THREE.FlatShading
+    });
+
+    var leftArm = new THREE.Mesh(armGeometry, armMaterial);
+    leftArm.name = "P1 Left Arm";
+    leftArm.position.x = 3;
+    leftArm.position.y = 0;
+    leftArm.position.z = 6;
+    
+    leftArm.castShadow = true;
+    leftArm.receiveShadow = true;
+    this.collider.getNode().add(leftArm);
+
+    var rightArm = new THREE.Mesh(armGeometry, armMaterial);
+    rightArm.name = "P1 Left Arm";
+    rightArm.position.x = 3;
+    rightArm.position.y = 0;
+    rightArm.position.z = -6;
+    
+    rightArm.castShadow = true;
+    rightArm.receiveShadow = true;
+    this.collider.getNode().add(rightArm);
 
     //attacks
     this.attacks['lightpunch'] = UpperCut;
